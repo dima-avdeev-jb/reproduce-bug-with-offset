9992f7fe34610b8e8dedba0ad27a08b25976e1c4
dima.avdeev <dima.avdeev@jetbrains.com>
2022-11-28 09:21:15 +0400

    good vs bad

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,13 +1,6 @@
-import androidx.compose.material.MaterialTheme
-import androidx.compose.desktop.ui.tooling.preview.Preview
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.Button
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.*
@@ -15,6 +8,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.DpOffset
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
@@ -22,20 +16,48 @@ import kotlinx.coroutines.delay
 
 fun main() = application {
     Window(onCloseRequest = ::exitApplication) {
-        var offset by remember { mutableStateOf(DpOffset.Zero) }
+        ReproduceOffsetBug()
+    }
+}
 
-        LaunchedEffect(Unit) {
-            repeat(200) {
-                delay(10)
-                offset -= DpOffset(10.dp, 0.dp)
-            }
+@Composable
+fun ReproduceOffsetBug() {
+    var offsetGood by remember { mutableStateOf(DpOffset.Zero) }
+    var offsetBad by remember { mutableStateOf(IntOffset.Zero) }
+
+    LaunchedEffect(Unit) {
+        repeat(200) {
+            delay(10)
+            offsetGood -= DpOffset(10.dp, 0.dp)
+            offsetBad -= IntOffset(20, 0)
         }
+    }
 
+    Column(modifier = Modifier.fillMaxSize()) {
         Box(
             modifier = Modifier
-                .fillMaxSize()
+                .fillMaxWidth()
+                .height(100.dp)
                 .background(Color.Red)
-                .offset(offset.x, offset.y)
+                .offset(offsetGood.x, offsetGood.y)
+                .background(Color.Blue)
+        ) {
+            repeat(20) {
+                Text(
+                    text = "$it",
+                    modifier = Modifier
+                        .size(50.dp)
+                        .offset(100.dp * it, 0.dp)
+                        .background(Color.Green),
+                )
+            }
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .background(Color.Red)
+                .offset { offsetBad }
                 .background(Color.Blue)
         ) {
             repeat(20) {
